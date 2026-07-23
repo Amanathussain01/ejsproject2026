37228f7f7b04e411d884336577b5b7145110babc
Amanathussain01 <Amanat.asdf@gmail.com>
2026-07-23 21:42:13 +0530

    db url change

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connect(){
     try {
-        await mongoose.connect('mongodb://localhost:27017/mern2026frontend')
+        await mongoose.connect('mongodb+srv://reelo_db_user:Amanat123%40@cluster0.iyrbpdy.mongodb.net/list_db')
         console.log("db connected......")
     } catch (err) {
         console.log(err)
